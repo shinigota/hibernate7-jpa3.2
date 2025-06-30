b1b08d457475adaa38c6931c17a7453e7b600c47
Benjamin Barbe <ben.barbe10@gmail.com>
2025-07-01 00:16:53 +0200

    Reword

--- src/main/java/com/sqli/hibernate7/dao/ShopDao.java
+++ src/main/java/com/sqli/hibernate7/dao/ShopDao.java
@@ -27,27 +27,27 @@ public class ShopDao extends AbstractDao<Shop, Long>{
     public List<Shop> findAllByOwnerId(Long ownerId) {
 
 
-        List<Shop> shops = this.sessionFactory.callInTransaction(em -> {
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Shop> cr = cb.createQuery(Shop.class);
-            Root<Shop> root = cr.from(Shop.class);
-            cr.select(root);
-            cr.where(
-                    cb.and(
-                            cb.equal(root.get(Shop_.owner), ownerId),
-                            cb.equal(root.get(Shop_.address).get(Address_.city), "Pessac")));
-
-            return em
-                    .createQuery(cr)
-                    .setHint("jakarta.persistence.fetchgraph", em.getEntityGraph(Shop_.GRAPH_SHOP_WITH_EMPLOYEES_NATIVE_JPA))
-                    .getResultList();
-
-        });
-//        List<Shop> shops =  this.sessionFactory.callInTransaction( em ->
-//                em.createNamedQuery(Shop_.QUERY_SHOP_FIND_ALL_BY_OWNER_ID)
-//                        .setParameter("id", ownerId)
-//                        .setHint("jakarta.persistence.fetchgraph", "Shop.withEmployees")
-//                        .getResultList());
+//        List<Shop> shops = this.sessionFactory.callInTransaction(em -> {
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Shop> cr = cb.createQuery(Shop.class);
+//            Root<Shop> root = cr.from(Shop.class);
+//            cr.select(root);
+//            cr.where(
+//                    cb.and(
+//                            cb.equal(root.get(Shop_.owner), ownerId),
+//                            cb.equal(root.get(Shop_.address).get(Address_.city), "Pessac")));
+//
+//            return em
+//                    .createQuery(cr)
+//                    .setHint("jakarta.persistence.fetchgraph", em.getEntityGraph(Shop_.GRAPH_SHOP_WITH_EMPLOYEES_NATIVE_JPA))
+//                    .getResultList();
+//
+//        });
+        List<Shop> shops =  this.sessionFactory.callInTransaction( em ->
+                em.createNamedQuery(Shop_.QUERY_SHOP_FIND_ALL_BY_OWNER_ID)
+                        .setParameter(Shop_.ID, ownerId)
+                        .setHint("jakarta.persistence.fetchgraph", "Shop.withEmployees")
+                        .getResultList());
 
         return shops;
     }
